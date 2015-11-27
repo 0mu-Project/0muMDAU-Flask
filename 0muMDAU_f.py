c0be7ce3cc6c50fdfd9745fbbff63883170cec87
m85091081 <b10216041@yuntech.edu.tw>
2015-11-27 17:46:16 +0800

    flask verion port

--- 0muMDAU_f.py
+++ 0muMDAU_f.py
@@ -4,6 +4,7 @@ import git
 import os
 import setting
 import hashlib
+import subprocess
 
 app = Flask(__name__)
 
@@ -14,10 +15,10 @@ def index():
         if os.path.exists(blogpath) == True: 
             g = git.cmd.Git()
             g.pull()
-            return redirect(url_for('loginp'))
+            return 'OK'
         else:
             git.Git().clone(setting.gitpath)
-            return redirect(url_for('loginp'))
+            return 'OK'
     else:
         return render_template('gitload.html')
 
@@ -49,28 +50,101 @@ def edit():
     if 'username' in session:
         return render_template('redit.html')
 
-@app.route('/jsonlist/<list>')
-def jsonlist(list):
-    if list == "posts" :
-        postpath = "_posts"
-    else:
-        postpath = "blog/_posts"
-    
-    if os.path.exists(postpath) == True:
-        directory = os.path.expanduser(postpath)
-        data = []
-        i = 0
-        for f in os.listdir(directory):
-            if os.path.isfile(os.path.join(directory, f)):
-                i = i + 1
-                data.insert(i,f)
-            import json
-            jsondump = json.dumps(data,separators=( ',' , ':'))
+@app.route('/save' , methods=['GET','POST'])
+def save():
+    if request.method == "POST":
+        argment = request.form['content']
+        fil =  request.form['title']
+        import datetime
+        now = datetime.datetime.now()
+        filen = now.strftime("%Y-%m-%d")+"-"+fil
+
+        if not fil.strip():
+            return "打標題啦,e04!"
+        else:
+            f = open('_posts/'+ filen  +'.markdown', 'wb') 
+            f.write(argment.encode('UTF-8'))
+            f.close()
+            ans = "file open"
+            return "文章已經存在本地的_posts,重新整理即可在佇列中看到"
+
+@app.route('/submit' , methods=['GET','POST'])
+def submit():
+    if request.method == "POST":
+        argment = request.form['content']
+        fil = request.form['title']
+        import datetime
+        now = datetime.datetime.now()
+        filen = now.strftime("%Y-%m-%d")+"-"+fil 
+        if not fil.strip():
+            return "打檔名拉,e04"
+        else:
+            user = request.form['username']
+            passd = request.form['password']
+            pathuser = "../pskey/" + user
+            hashsha =  hashlib.sha256(passd.replace('\n','').encode())
+            if os.path.exists(pathuser) == True :
+                with open( pathuser ,'r' ) as f:
+                    fline = f.readline()
+                    if fline.replace('\n' , '')  == hashsha.hexdigest() :
+                        f = open('_posted/'+ filen  +'.markdown', 'wb') 
+                        f.write(argment.encode('UTF-8'))
+                        f.close()
+                        ans = "file open"
+                        import shutil
+                        shutil.copyfile('_posted/' + filen +'.markdown','blog/_posts/' + filen +'.markdown')
+                        repo = git.Repo("./blog")
+                        index = repo.index
+                        index.add(["_posts"])
+                        message = 'add new posts' + filen
+                        index.commit(message)
+                        subprocess.call(['bash ./script/autoAuth.sh ' + user + ' ' + passd + ' ./blog'], shell=True)
+                        return "文章已經存在本地的_posted,文章即將發布.." 
+                    else:
+                        return "密碼錯誤是要登入三小"
+            else:
+                return "帳號錯誤是要登入三小"
+        
+@app.route('/getmd/<listmd>', methods=['GET','POST'])
+def markdownr(listmd):
+    if request.method == "POST":
+        f = open('_posts/' + listmd)
+        return f.read()
+
+
+@app.route('/getmdposted/<listposed>', methods=['GET','POST'])
+def markdownrp(listposed):
+    if request.method == "POST":
+        f = open('blog/_posts/'+ name)
+        return f.read()
+
+
+
+@app.route('/jsonlist/<lists>')
+def jsonlist(lists):
+    if 'username' in session:
+        if lists == "posts" :
+            postpath = "_posts"
+        else:
+            postpath = "blog/_posts"
+            
+        if os.path.exists(postpath) == True:
+            directory = os.path.expanduser(postpath)
+            data = []
+            i = 0
+            for f in os.listdir(directory):
+                if os.path.isfile(os.path.join(directory, f)):
+                    i = i + 1
+                    data.insert(i,f)
+                import json
+                jsondump = json.dumps(data,separators=( ',' , ':'))
             resp = Response(response=jsondump,status=200, mimetype="application/json")
+            return(resp)
+        else:
+            os.makedirs(postpath)
+        return 'OK'
     else:
-        os.makedirs(postpath)
-        resp =""
-    return(resp)
+        return 'Eat Shit Please!'
 
 
 app.secret_key = 'aoksp=f^=qrt%%%___jrfw'
