ba56cf8127a2c7a120c88d0b6bce26c719834e50
m85091081 <b10216041@yuntech.edu.tw>
2015-12-01 19:01:19 +0800

    add git pull

--- envri.py
+++ envri.py
@@ -25,6 +25,7 @@ if __name__ == "__main__":
     while 1:
         if portcheck(9002) == True:
             if os.path.exists("_posted") == True :
+                rungitpull()
                 importapp()
             else:
                 os.makedirs("_posted")
