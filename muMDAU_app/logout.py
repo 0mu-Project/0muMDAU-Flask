e2d152e3d592c42521a8b150a38895ce3fa58431
m85091081 <b10216041@yuntech.edu.tw>
2015-12-19 14:53:33 +0800

    modify logout

--- muMDAU_app/logout.py
+++ muMDAU_app/logout.py
@@ -4,4 +4,4 @@ from flask import session,redirect,url_for
 @app.route('/logout')
 def logout():
     session.pop('username', None)
-    return redirect(url_for('index'))
+    return redirect(url_for('main.index'))
