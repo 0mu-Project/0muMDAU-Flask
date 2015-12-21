4b3b5653a413c44b3409e34952244e24f4cef8a4
m85091081 <b10216041@yuntech.edu.tw>
2015-12-22 06:30:27 +0800

    modify git clone log to log file

--- muMDAU_app/index.py
+++ muMDAU_app/index.py
@@ -1,6 +1,7 @@
 from muMDAU_app import app , setting
 from flask import request , render_template , Blueprint
 import subprocess , os
+from subprocess import PIPE
 
 main = Blueprint('main',__name__)
 
@@ -9,11 +10,18 @@ def index():
     if request.method == 'POST':
         blogpath = "./blog"
         if os.path.exists(blogpath) == True:
-            subprocess.call(['git pull'], shell=True , cwd='./blog')
+            p = subprocess.Popen(['git pull'], shell=True , cwd='./blog',stdout=PIPE)
+            outcode , error = p.communicate()
+            f = open(setting.s_log,'ab')
+            f.write(str.encode('INFO:pull_blog:git -- ') + outcode)
+            f.close()
             return 'OK'
         else:
-            subprocess.call(['git clone ' + setting.gitpath], cwd='./',shell=True)
-            print("clone")
+            p = subprocess.Popen(['git clone ' + setting.gitpath], cwd='./',shell=True , stdout = PIPE,stderr=PIPE )
+            outcode , gitlog = p.communicate()
+            f = open(setting.s_log,'ab')
+            f.write(str.encode('INFO:cloneblog:git -- ') + gitlog )
+            f.close()
             return 'OK clone'
     else:
         return render_template('gitload.html')
