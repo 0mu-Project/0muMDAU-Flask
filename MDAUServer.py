744fa0ac06aa9561e8b4b37f6fb63f04c3ad0f5b
m85091081 <b10216041@yuntech.edu.tw>
2016-02-10 12:42:50 +0800

    add commit

--- MDAUServer.py
+++ MDAUServer.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 0muMDAU Server
 from muMDAU_app import app 
 import logging, setting
 from muMDAU_app.index import main
@@ -5,18 +8,22 @@ from muMDAU_app.editor import peditor
 from muMDAU_app.editor import markdown
 from werkzeug.contrib.fixers import ProxyFix 
 
+# muMDAU_app setting 
 app.secret_key = setting.yourkey
 app.wsgi_app = ProxyFix(app.wsgi_app)
 app.register_blueprint(peditor, url_prefix='/edit')
 app.register_blueprint(markdown, url_prefix='/md')
 app.register_blueprint(main)
 
+# Main function of MDAUServer
 if __name__ == '__main__':
+    # log writeing
     logging.basicConfig(filename=setting.s_log, level=logging.DEBUG)
     print('0MuMDAU Server Run on ' + str(setting.host) + ':' + str(setting.port))
+    # check debug
     if setting.debug == 0:
         debugB = False 
     else:
         debugB = True
-        print('fuu')
+        print('Debug Mode is run!')
     app.run(host=str(setting.host), port=setting.port, debug=debugB)
--- envri.py
+++ envri.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# 0muWatchdog runtime environment
 import setting
 import sys
 import os
@@ -7,8 +8,10 @@ import socket
 import subprocess
 from database import InitDB
 
+# check version_info of python 
 def checkenvir():
     if sys.version_info[0] == 3:
+        # if use pypy3 will have this sys builtin
         is_pypy = '__pypy__' in sys.builtin_module_names
         if is_pypy is True:
             try:
@@ -19,6 +22,7 @@ def checkenvir():
                     import pip  # NOQA
                     print('You dont have flask install , Auto Install!')
                     print('Please enter root password !')
+                    # call subprocess to install unfound modules
                     subprocess.call(['sudo pypy3 -m pip install Flask'], shell=True)
                     subprocess.call(['sudo pypy3 -m pip install six'], shell=True)
                 except ImportError:
@@ -31,19 +35,23 @@ def checkenvir():
                 return 'python3'
             except ImportError:
                 print('You dont have flask install , please input your password to install flask . ')
+                # call subprocess to install unfound modules
                 subprocess.call(['sudo python3 -m pip install Flask'], shell=True)
                 subprocess.call(['sudo python3 -m pip install six'], shell=True)
                 return 'python3'
     else:
         return 'python2'
 
+# run master branch git pull to update server
 def rungitpull():
     print('# Git status')
     subprocess.call(['git pull'], shell=True)
 
+# use subprocess to call MDAUServer
 def importapp():
     subprocess.call([checkenvir() + ' MDAUServer.py'], shell=True)
 
+# use socket to check port open
 def portcheck(port):
     s = socket.socket()
     s.settimeout(0.5)
@@ -52,10 +60,13 @@ def portcheck(port):
     finally:
         s.close()
 
+# DB Init
 def preDB():
     InitDB.createTable()
 
+# Main class 
 if __name__ == '__main__':
+    # loop for all time
     while 1:
         if os.path.exists(setting.sqliteFile):
             print('Now 0mu-WatchDog is run')
